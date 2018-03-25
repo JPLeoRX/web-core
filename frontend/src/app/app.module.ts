06e41fad50f7c2f9382490b185ff196497cfeffb
Leo Ertuna <leo.ertuna@gmail.com>
2018-03-25 19:08:27 +0300

    Fixed reactive forms module & dialog components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,20 +4,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
-import {SharedModule} from "./shared/shared.module";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {AppRoutingModule} from "./app.routing";
+import { SharedModule } from "./shared/shared.module";
+import { AppRoutingModule } from "./app.routing";
+import { ExampleAddDialogComponent } from './example/example-add-dialog/example-add-dialog.component';
+import { ExampleEditDialogComponent } from './example/example-edit-dialog/example-edit-dialog.component';
+import { ReactiveFormsModule } from "@angular/forms";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent
+    ExampleComponent,
+    ExampleAddDialogComponent,
+    ExampleEditDialogComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
+    ReactiveFormsModule,
     AppRoutingModule
+  ],
+  entryComponents: [
+    ExampleAddDialogComponent,
+    ExampleEditDialogComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
