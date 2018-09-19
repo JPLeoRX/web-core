d8acfca46ea82ff033292d38ae99e366515718bf
Leo <leo.ertuna@gmail.com>
2018-09-19 11:03:22 +0300

    Moved all entities to corresponding module

---
+++ entities/src/main/java/io/github/jpleorx/webcore/entities/RestApiObject.java
@@ -0,0 +1,25 @@
+package io.github.jpleorx.webcore.entities;
+
+import io.github.jpleorx.webcore.entities.id.AbstractId;
+
+/**
+ * This is the parent class for all Rest Api Objects (objects presented to client)
+ *
+ * @param <I> entity's id (although in this object it will be used as string, we need this field just to allow proper class matching)
+ *
+ * @author Leo Ertuna
+ * @since 17.05.2018 14:44
+ */
+public interface RestApiObject<I extends AbstractId> extends Entity<I> {
+    /**
+     * Getter for id
+     * @return
+     */
+    String getId();
+
+    /**
+     * Setter for id
+     * @param newId
+     */
+    void setId(String newId);
+}
---
+++ entities/src/main/java/io/github/jpleorx/webcore/entities/properties/HasId.java
@@ -0,0 +1,26 @@
+package io.github.jpleorx.webcore.entities.properties;
+
+import io.github.jpleorx.webcore.entities.id.AbstractId;
+
+/**
+ * This is a marker interface. It shows that this object has an ID.
+ * In this interface we force the object to provide getters and setters for this property.
+ *
+ * @param <I> id type of the object
+ *
+ * @author Leo Ertuna
+ * @since 26.05.2018 17:50
+ */
+public interface HasId<I extends AbstractId> {
+    /**
+     * Getter for id
+     * @return id
+     */
+    I getId();
+
+    /**
+     * Setter for id
+     * @param id id
+     */
+    void setId(I id);
+}
